8b587bd40d1bd29b875a1b4f1d26e5a2c938d45d
Daniel Nicolai <dalanicolai@gmail.com>
2022-09-26 14:30:35 +0200

    Rename to doc-mupdf

--- doc-mupdf.el
+++ doc-mupdf.el
@@ -1,20 +1,20 @@
 ;; -*- lexical-binding: t; -*-
-(defvar mupdf-info-commands '(mupdf-page-sizes
-                              mupdf-structured-contents))
+(defvar doc-mupdf-info-commands '(doc-mupdf-page-sizes
+                              doc-mupdf-structured-contents))
 
-(defun mupdf-info (function &optional arg)
+(defun doc-mupdf-info (function &optional arg)
   (interactive (if (member (file-name-extension (buffer-file-name))
                            '("pdf" "epub"))
                    (list (completing-read "Select info type: "
-                                          mupdf-info-commands)
+                                          doc-mupdf-info-commands)
                          current-prefix-arg)
                  (user-error "Buffer file not of `pdf' or `epub' type")))
   (pp (funcall (intern-soft function))
       (when arg
-        (get-buffer-create "*mupdf-info*")))
-  (when arg (pop-to-buffer "*mupdf-info*")))
+        (get-buffer-create "*doc-mupdf-info*")))
+  (when arg (pop-to-buffer "*doc-mupdf-info*")))
 
-(defun mupdf-page-sizes (&optional file)
+(defun doc-mupdf-page-sizes (&optional file)
   (setq file (or file buffer-file-name))
   (pcase (file-name-extension file)
 
@@ -22,7 +22,7 @@
                                   (file-name-as-directory (file-name-base file))
                                   "thumbs/")))
               (unless (file-exists-p outdir)
-                (mupdf-create-thumbs))
+                (doc-mupdf-create-thumbs))
               (let* ((size-string (cadr (split-string
                                          (car (process-lines "file"
                                                              (concat outdir "thumb1.png")))
@@ -54,12 +54,12 @@
 ;;                                            lines))
 ;;                   (pages (or (string-to-number (cadr (nth 5 split-lines)))
 ;;                              (user-error "Output does not have 11 lines;
-;; the function `mupdf-page-sizes' should get modified/generalized")))
+;; the function `doc-mupdf-page-sizes' should get modified/generalized")))
 ;;                   (box-line (nth 9 split-lines)))
 ;;              (make-list pages (cons (string-to-number (nth 5 box-line))
 ;;                                     (string-to-number (nth 6 box-line))))))))
 
-(defun mupdf-get-image-data (page width &optional file)
+(defun doc-mupdf-get-image-data (page width &optional file)
   (setq file (or file (buffer-file-name)))
   (call-process "mutool" nil nil nil
                 "draw"
@@ -73,7 +73,7 @@
     (insert-file-contents-literally "/tmp/pdf-temp-img")
     (buffer-substring-no-properties (point-min) (point-max))))
 
-(defun mupdf-create-pages (width &optional file force)
+(defun doc-mupdf-create-pages (width &optional file force)
   (setq file (or file (buffer-file-name)))
   (let ((outdir (concat "/tmp/" (file-name-as-directory (file-name-base file)) "pages/")))
     (when (or (not (file-exists-p outdir)) force)
@@ -87,7 +87,7 @@
         (set-process-sentinel proc (lambda (process event)
                                      (message "Create pages process %s" event)))))))
 
-(defun mupdf-create-thumbs (&optional file force)
+(defun doc-mupdf-create-thumbs (&optional file force)
   (setq file (or file (buffer-file-name)))
   (let ((outdir (concat "/tmp/"
                         (file-name-as-directory (file-name-base file))
@@ -104,18 +104,18 @@
                                      (message "Create pages process %s" event)))))))
 
 
-(defun mupdf-parse-coords (coords-string)
+(defun doc-mupdf-parse-coords (coords-string)
   (mapcar #'string-to-number (split-string coords-string)))
 
-(defun mupdf-parse-line (line-contents)
-  (append (mupdf-parse-coords (alist-get 'bbox (nth 1 line-contents)))
+(defun doc-mupdf-parse-line (line-contents)
+  (append (doc-mupdf-parse-coords (alist-get 'bbox (nth 1 line-contents)))
           (list (mapcan (lambda (e)
                           (mapconcat (lambda (c)
                                        (alist-get 'c (nth 1 c)))
                                      (nthcdr 2 e)))
                         (nthcdr 2 line-contents)))))
 
-(defun mupdf-structured-contents (&optional detail file &rest pages)
+(defun doc-mupdf-structured-contents (&optional detail file &rest pages)
   (interactive)
   (setq file (or file (buffer-file-name)))
   (let (text)
@@ -134,9 +134,9 @@
     ;; (when (eq detail 'char)
     ;;   (setq text (mapcan (apply-partially #'nthcdr 2)
     ;;                      (mapcan (apply-partially #'nthcdr 2) text))))
-    ;; (mapcar #'mupdf-parse-line text)))
+    ;; (mapcar #'doc-mupdf-parse-line text)))
 
-(defun mupdf-outline (&optional file)
+(defun doc-mupdf-outline (&optional file)
   (setq file (or file (buffer-file-name)))
   (mapcar (lambda (l)
             (let* ((parts (split-string l "\\(#page=\\|&zoom\\)"))
